b8479d72d74f3fca74f99064640ccdc519aed627
swati_goyal@outlook.com <be9entia>
2020-02-29 12:56:17 +0530

    Few changes in requirements file

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, session, render_template, request, redirect, url_for, flash, jsonify
 import requests
-from flask_sessions import Session
+from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from unicodedata import normalize
